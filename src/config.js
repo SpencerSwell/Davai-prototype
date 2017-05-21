62bff23f1da7de2245d2d4d48908c5cca9cf2705
CodeforLiberty <sswell@live.com>
2017-05-21 08:50:11 -0400

    will it connect?

--- src/config.js
+++
@@ -1,3 +0,0 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                      'mongodb://Admin:dev@ds013414.mlab.com:13414/davai';
--- src/server.js
+++ src/server.js
@@ -1,10 +1,7 @@
 
-const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 
-const {DATABASE_URL} = require('./config.js');
-const userinfo = require('./schema.js');
 const planecharter = require('./schema.js');
 
 const app = express();
@@ -13,7 +10,7 @@ const app = express();
 
 mongoose.Promise = global.Promise;
 
-
+var connection = mongoose.connect('mongodb://Admin:dev@ds013414.mlab.com:13414/davai');
 
 // app.get('/getFlightData', (req, res) => {
 //   // console.log(req);
@@ -34,48 +31,10 @@ mongoose.Promise = global.Promise;
 
 
 
-// let server;
+// let server
+   
 
-// this function connects to our database, then starts the server
-function runServer() {
-  return new Promise((resolve, reject) => {
-    mongoose.connect(DATABASE_URL, err => {
-      if (err) {
-        return reject(err);
-      }
-      server = app.listen(PORT, () => {
-        console.log(`Your app is listening on port ${PORT}`);
-        resolve();
-      })
-      .on('error', err => {
-        mongoose.disconnect();
-        reject(err);
-      });
-    });
-  });
-}
+    mongoose.connect(DATABASE_URL);
 
-// this function closes the server, and returns a promise. we'll
-// use it in our integration tests later.
-function closeServer() {
-  return mongoose.disconnect().then(() => {
-     return new Promise((resolve, reject) => {
-       console.log('Closing server');
-       server.close(err => {
-           if (err) {
-               return reject(err);
-           }
-           resolve();
-       });
-     });
-  });
-}
 
-// if server.js is called directly (aka, with `node server.js`), this block
-// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
-if (require.main === module) {
-  runServer().catch(err => console.error(err));
-}
-
-module.exports = {runServer, app, closeServer};
 
